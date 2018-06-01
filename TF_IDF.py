f668f11c50e66a0cf8888934e96ba40233f1bd16
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-06-02 01:17:13 +0900

    TF_IDF.py 1. 파일읽기 2. 명사끼리 모으기 3. 명사의 빈도 구하기 4. 빈도수가 높은 순서대로 정렬하기 5. TF구하기

--- TF_IDF.py
+++ TF_IDF.py
@@ -1,5 +1,33 @@
+import re
+import operator
+import math
+
 # 파일 읽기
 file = open('./Input_Data/child/(POS)child_1.txt','r', encoding='utf8')
-data = file.read()
-print (data)
+data = file.readlines()
+
+# 명사끼리 모으기
+list = []
+for word in data:
+    p = re.compile('(/(NNP|NNG))')
+    m = re.finditer('[가-힣]+/(NN(G|P))',word)
+    if m :
+        for r in m:
+            list.append(p.sub("",r.group()))
+
+# 명사의 빈도
+dict ={}
+for word in list:
+    dict[word] = list.count(word)
+
+# 빈도수가 높은 순서대로 소팅(리스트 안의 튜플이 됨)
+dict = sorted(dict.items(), key = operator.itemgetter(0))
+
+# TF 구하기
+for i in range(0,len(dict)):
+    TF = round(math.log10(dict[i][1] + 1),4)
+    dict[i] = dict[i] + (TF,)
+print(dict)
+
+# 파일 닫기
 file.close()
