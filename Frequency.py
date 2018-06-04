bf56787b88c7b3a3c65776aa61ff847ba68778cd
Jaeseong Park(JSP) <tmakdlfwotjd@naver.com>
2018-06-05 01:03:07 +0900

    find 5000 words

--- Frequency.py
+++ Frequency.py
@@ -2,36 +2,64 @@ import re
 import operator
 import math
 
-# 파일 읽기
-
+# 카테고리 별 딕셔너리
 category = {"child": 129, "culture": 220, "economy": 167, "education": 121, "health": 192, "life": 113, "person": 173,
             "policy": 253, "society": 328}
 
+# 전체 카테고리 별 빈도 수 딕셔너리
 ca_dict = {}
 
+# 모든 단어 리스트
+total_list = []
+
+print("<카테고리 별 단어 빈도수>")
 for ca in category:
     word_list = []
     for index in range(1, category[ca]):
         file = open('./Input_Data/' + ca + '/(POS)' + ca + '_' + str(index) + '.txt', 'r', encoding='utf8')
         data = file.readlines()
 
+        # 명사만 뽑아 내기
         for word1 in data:
             p = re.compile('(/(NNP|NNG))')
             m = re.finditer('[가-힣]+/(NN(G|P))', word1)
             if m:
                 for r in m:
                     word_list.append(p.sub("", r.group()))
+                    total_list.append(p.sub("", r.group()))
         file.close()
 
+    # 카테고리 별 단어의 빈도 수
     word_count = {}
-    for word2 in word_list:
-        word_count[word2] = word_list.count(word2)
-    word_count = sorted(word_count.items(), key=operator.itemgetter(0))
-    ca_dict[ca] = word_count
-    print(ca + " finish")
 
-for index in ca_dict:
-    print(ca_dict[index])
+    # 카테고리 별 단어의 빈도 수 딕셔너리로 만들기
+    for word2 in word_list:
+        if word2 in word_count.keys():
+            continue;
+        word_count[word2] = word_list.count(word2)
+
+    # 파일 읽는데 걸리는 시간을 없애기 위해
+    print("--" + ca + "--")
+    print(word_count)
+
+    ca_dict[ca] = word_count
+
+# 전체 단어의 빈도수 딕셔너리
+total_count = {}
+
+for word3 in total_list:
+    if word3 in total_count.keys():
+        continue;
+    total_count[word3] = total_list.count(word3)
+
+# 전체 단어 빈도수 딕셔너리 출력
+print("전체 문서 단어의 빈도수 출력")
+print(total_count)
+
+# 등장하는 모든 단어 출력
+total_list = list(set(total_list))
+print("전체 문서 단어 출력")
+print(total_list)
 
 ''' data = file.readlines()
 # 명사끼리 모으기
@@ -50,9 +78,6 @@ for word in list:
 
 # 빈도수가 높은 순서대로 소팅(리스트 안의 튜플이 됨)
 dict = sorted(dict.items(), key = operator.itemgetter(0))
-
-
-
 
 # TF 구하기
 for i in range(0, len(dict)):
